cf20d013ad22fc2bc332784281a8cfc7e66a4eba
cphsiung <chunpeihs@gmail.com>
2020-07-19 23:12:02 -0400

    Update numbers_problem_v3 loop.rb

--- numbers_problem_v3 loop.rb
+++ numbers_problem_v3 loop.rb
@@ -1,6 +1,7 @@
 puts "Let's play a numbers game. How many numbers would you like to enter?"
 input = gets.chomp.to_i
 
+# ordinalize numbers to 1st, 2nd, 3rd...etc
 class Integer
   def ordinalize
     case self%10
@@ -16,14 +17,26 @@ class Integer
   end
 end
 
-i = 1
-while i <= input
-  puts "Enter the #{i.ordinalize} positive integer:"
+# using times loop
+input.times do |i|
+  puts "Enter the #{(i + 1).ordinalize} positive integer:"
   number = gets.chomp.to_i
     if number % 3 == 0
       puts "#{number} is divisible by 3."
     else number % 3 != 0
       puts "#{number} is not divisible by 3."
     end
-  i += 1
 end
+
+# using while loop
+# i = 1
+# while i <= input
+#   puts "Enter the #{i.ordinalize} positive integer:"
+#   number = gets.chomp.to_i
+#     if number % 3 == 0
+#       puts "#{number} is divisible by 3."
+#     else number % 3 != 0
+#       puts "#{number} is not divisible by 3."
+#     end
+#   i += 1
+# end
